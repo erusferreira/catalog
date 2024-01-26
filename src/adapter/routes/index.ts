bd756a0e2c23fe4f2c0240b28a35fae634d95283
Érus Ferreira <erusferreira@gmail.com>
2024-01-26 14:13:45 -0300

    Ignore favicon

--- src/adapter/routes/index.ts
+++ src/adapter/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { ignoreFavicon } from '../utils/ignoreFavicon'
+const favicon = require('serve-favicon');
+const path = require('path');
+
 import catalogRouter from './catalog';
 import categoryRouter from './category';
 import healthRouter from './health';
@@ -8,9 +10,8 @@ import itemRouter from './item';
 import authRouter from './auth';
 import userRouter from './user';
 
-
 const routes = Router();
-routes.use(ignoreFavicon);
+routes.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 routes.use(authRouter);
 routes.use(healthRouter);
 routes.use(catalogRouter);
