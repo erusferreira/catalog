4457c3f79d36face78f5320d670ebc78254547d3
Érus Ferreira <erusferreira@gmail.com>
2024-01-26 14:06:34 -0300

    Ignore favicon

--- src/adapter/utils/ignoreFavicon.ts
+++ src/adapter/utils/ignoreFavicon.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 
-export function ignoreFavicon(req: Request, res: Response, next: NextFunction) {
-  if (req.originalUrl.includes('favicon.ico')) {
-    res.status(204).end()
+export const ignoreFavicon = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (req.originalUrl.includes('favicon.ico')) {
+      res.status(204).end()
+    }
+    next();
+  } catch (error: any) {
+    res.status(404).json({message: error.message});
   }
-  next();
 }
